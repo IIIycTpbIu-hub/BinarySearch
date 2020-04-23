6ed8e4f154ac253ca0218eb01cfb284e8fcac492
IIIycTpbIu-hub <and95rey@yandex.ru>
2020-04-23 19:32:08 +0400

    Initial commit

---
+++ SortedArrayProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SortedArrayProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
