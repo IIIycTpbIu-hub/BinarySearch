71e3b3a13c39dbb164839417de066d866f953926
IIIycTpbIu-hub <and95rey@yandex.ru>
2020-04-25 18:30:47 +0400

    Complete

--- SortedArrayProject/Program.cs
+++ SortedArrayProject/Program.cs
@@ -10,52 +10,63 @@ namespace SortedArrayProject
             Stopwatch simpleArrayStopWatch = new Stopwatch();
             Stopwatch orderedArrayStopWatch = new Stopwatch();
 
-            int count = 100000;
+            int count = 320000;
             Console.WriteLine("Создаем массивы из {0} элементов", count);
             int[] sipmleArray = new int[count];
 
             OrderedArray<Int32> orderedArray = new OrderedArray<int>(count);
-
+            int progress = 0;
+            int step = count/100;
             Random random = new Random();
-            int millisecondsSimple = 0;
-            int millisecondsOrdered = 0;
             for (int i = 0; i < count; i++)
             {
                 int val = random.Next(0, count);
                 simpleArrayStopWatch.Start();
                 sipmleArray[i] = val;
                 simpleArrayStopWatch.Stop();
-                //millisecondsSimple += simpleArrayStopWatch.Elapsed.Milliseconds;
 
                 orderedArrayStopWatch.Start();
                 orderedArray.Add(val);
                 orderedArrayStopWatch.Stop();
-                //millisecondsOrdered += orderedArrayStopWatch.Elapsed.Milliseconds;
+
+                if (i - progress == step)
+                {
+                    progress += step;
+                    Console.Write(progress/ step + "% .. ");
+                }
+                
             }
-            Console.WriteLine("Заполнение обычного массива из {0} элементов заняло {1} секунд {2} милисекунд", count, simpleArrayStopWatch.Elapsed.Seconds, simpleArrayStopWatch.Elapsed.Milliseconds);
-            Console.WriteLine("Заполнение отсортированного массива из {0} элементов заняло {1} секунд {2} милисекунд", count, orderedArrayStopWatch.Elapsed.Seconds, orderedArrayStopWatch.Elapsed.Milliseconds);
+            Console.WriteLine("Заполнение обычного массива из {0} элементов заняло {3} минут {1} секунд {2} милисекунд", count, simpleArrayStopWatch.Elapsed.Seconds, simpleArrayStopWatch.Elapsed.Milliseconds, simpleArrayStopWatch.Elapsed.Minutes);
+            Console.WriteLine("Заполнение отсортированного массива из {0} элементов заняло {3} минут {1} секунд {2} милисекунд", count, orderedArrayStopWatch.Elapsed.Seconds, orderedArrayStopWatch.Elapsed.Milliseconds, orderedArrayStopWatch.Elapsed.Minutes);
 
             orderedArrayStopWatch.Reset();
             simpleArrayStopWatch.Reset();
 
-            int searchingItemsCount = 100000;
+            int searchingItemsCount = count;
             Console.WriteLine("Поиск случайных элементов",searchingItemsCount);
+            progress = 0;
             for (int i = 0; i < searchingItemsCount; i++)
             {
                 int val = random.Next(0, count);
-                for (int j = 0; j < count; j++)
-                {
-                    simpleArrayStopWatch.Start();
-                    if (sipmleArray[j] == val)
-                    {
-                        simpleArrayStopWatch.Stop();
-                        break;
-                    }
-                }
+                //simpleArrayStopWatch.Start();
+                //for (int j = 0; j < count; j++)
+                //{
+                //    if (sipmleArray[j] == val)
+                //    {
+                //        break;
+                //    }
+                //}
+                //simpleArrayStopWatch.Stop();
 
                 orderedArrayStopWatch.Start();
                 orderedArray.Find(val);
                 orderedArrayStopWatch.Stop();
+
+                if (i - progress == step)
+                {
+                    progress += step;
+                    Console.Write(progress / step + "% .. ");
+                }
             }
 
             Console.WriteLine("Поиск {0} случайных элементов в неупорядоченном массиве занял {3} минут {1} секунд {2} милисекунд"
